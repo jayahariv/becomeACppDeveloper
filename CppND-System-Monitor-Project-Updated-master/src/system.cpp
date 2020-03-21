2674e07dfd17c7aa1ca4696c49ec05789e47bc00
jayahariv <jayahariv88@gmail.com>
2020-03-21 14:34:12 -0700

    feat: Process
    * create process instance and return
    * process populated with pid and user name

--- CppND-System-Monitor-Project-Updated-master/src/system.cpp
+++ CppND-System-Monitor-Project-Updated-master/src/system.cpp
@@ -20,7 +20,13 @@ System::System() : kernel_(LinuxParser::Kernel()), os_(LinuxParser::OperatingSys
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() { 
+    vector<int> pids = LinuxParser::Pids();
+    for (int pid: pids) {
+        processes_.push_back(Process(pid));
+    }
+    return processes_; 
+}
 
 std::string System::Kernel() { return kernel_; }
 
